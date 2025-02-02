c926b09813080abeef703e92eda394e8b8008f9e
chaeyeonko <chaen0225@gmail.com>
2025-02-02 20:39:34 +0900

    chapter06(sort) 정렬 두 배열의 원소교체 정렬 사용한 풀이로 수정

--- chapter06_sort/6-12.py
+++ chapter06_sort/6-12.py
@@ -7,9 +7,19 @@ n, k = map(int, input().split())
 arr_1 = list(map(int, input().split()))
 arr_2 = list(map(int, input().split()))
 
+# for i in range(k):
+#     x = arr_1.index(min(arr_1))
+#     y = arr_2.index(max(arr_2))
+#     arr_1[x], arr_2[y] = arr_2[y], arr_1[x]
+
+
+# 정렬 사용하여 풀기
+
+arr_1 = sorted(arr_1)
+arr_2 = sorted(arr_2, reverse=True)
+
 for i in range(k):
-    x = arr_1.index(min(arr_1))
-    y = arr_2.index(max(arr_2))
-    arr_1[x], arr_2[y] = arr_2[y], arr_1[x]
+    if arr_1[i] < arr_2[i]:
+        arr_1[i], arr_2[i] = arr_2[i], arr_1[i]
 
 print(sum(arr_1))
