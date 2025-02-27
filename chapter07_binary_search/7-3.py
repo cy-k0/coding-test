8b8b6f9341e22b75758c549b72438cc0e61ccfe8
chaeyeonko <chaen0225@gmail.com>
2025-02-27 23:17:02 +0900

    chapter07(binary_search) 연습문제 7-3, 이진탐색 반복문 구현

---
+++ chapter07_binary_search/7-3.py
@@ -0,0 +1,21 @@
+# 반복문으로 이진탐색 구현
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+        
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("찾는 원소가 배열 내에 존재하지 않습니다")
+else:
+    print(result + 1)
