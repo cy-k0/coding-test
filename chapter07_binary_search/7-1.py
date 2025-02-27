942fb3dc9830ee47902ec57d2e03ba2195502e7f
chaeyeonko <chaen0225@gmail.com>
2025-02-27 22:39:21 +0900

    chapter07(binary_search) 연습문제 7-1

---
+++ chapter07_binary_search/7-1.py
@@ -0,0 +1,15 @@
+# 찾고자 하는 문자열 인덱스 반환
+
+
+def sequential_search(n, target, array):
+    for i in range(n):
+        if array[i] == target:
+            return i + 1
+
+
+input_data = input().split()
+n = int(input_data[0])
+target = input_data[1]
+array = input().split()
+
+print(sequential_search(n, target, array))
